2a521d010b4fcc701f971e72b993233b22598e2a
dingxy-hikari <dingx9320@gmail.com>
2024-02-09 14:57:12 +0800

    2024 the eve update

--- FOPs_main.py
+++ FOPs_main.py
@@ -4,45 +4,76 @@ import sys
 
 import PyQt5.QtWidgets as Qtw
 from PyQt5 import uic
+from PyQt5.QtGui import QFont
 
-M_GUI = False
-ans_list = ['A', "A", "A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D", ]
+M_GUI = True
+ans_dict = {
+    '15-4': {'lt': ['A', "A", "A", "A", "B", "B", "B", "B", "C", "C", "C", "C", "D", "D", "D", "D", ], 'tm': 15},
+    '10-4': {'lt': ['A', "A", "A", "B", "B", "B", "C", "C", "C", "D", "D", "D", ], 'tm': 10}}
 
 
 class UI(Qtw.QWidget):
     def __init__(self):
         super().__init__()
+        self.font = QFont()
+        self.Btn_CSM = None
+        self.Lb_SHW = None
+        self.Tb_SHW = None
+        self.Btn_CHG = None
+        self.Btn_SPW = None
+        self.Btn_QT = None
         self.ui = None
+        self.font.setPointSize(20)
         self.init_ui()
 
     def init_ui(self):
-        self.ui = uic.load("FOPs_GUI.ui")
+        self.ui = uic.loadUi("FOPs_GUI.ui")
+        self.Btn_SPW = self.ui.Btn_SPW
+        self.Btn_CHG = self.ui.Btn_CHG
+        self.Btn_CSM = self.ui.Btn_CSM
+        self.Btn_QT = self.ui.Btn_QT
+        self.Lb_SHW = self.ui.Lb_SHW
+        self.Tb_SHW = self.ui.Tb_SHW
+        self.Tb_SHW.setFont(self.font)
+        self.Btn_SPW.clicked.connect(self.g_spawn)
+        self.Btn_QT.clicked.connect(quit)
+
+    def g_spawn(self):
+        self.Tb_SHW.setText((ans_spawn(ans_dict['15-4']['lt'], ans_dict['15-4']['tm'])))
+        pass
+
+    def g_temp(self):
+        pass
 
 
 def ans_spawn(lt, tm):
     t_list = lt.copy()
-    outputting = ''
+    opt = ''
     ct = 0
     i = 0
     for i in range(tm):
         if ct == 5:
-            outputting += ' '
+            opt += ' '
             ct = 0
 
-        b = rd.randint(0, len(t_list)-1)
-        outputting += t_list[b]
+        b = rd.randint(0, len(t_list) - 1)
+        opt += t_list[b]
         del t_list[b]
         i += 1
         ct += 1
-    return outputting
+    return opt
 
 
 if __name__ == "__main__":
-    print(sys.argv)
     if M_GUI is True:
-        pass
+        app = Qtw.QApplication(sys.argv)
+        gui = UI()
+        gui.ui.show()
+        app.exec()
+        a = Qtw.QTextBrowser()
+
     elif M_GUI is False:
         while True:
             os.system('cls')
-            print(ans_spawn(ans_list, 15))
+            print(ans_spawn(ans_dict['15-4']['lt'], ans_dict['15-4']['tm']))
             os.system('pause')
